c4a3f0603ef69a8c5cce6e19ab4cc404f877ff22
Andrey Helldar <helldar@dragon-code.pro>
2023-02-26 19:07:39 +0300

    Improvement username parser

--- src/node/plugins/transformers/compare.ts
+++ src/node/plugins/transformers/compare.ts
@@ -6,9 +6,9 @@ export const compareCompact: GitHubLinkifyTransformer = (text: string, repo: str
     const replacerFull = (value, item) => value.replace(item[0], template('compare', `${ item[1] }/${ item[2] }`, item[3], item[4]))
     const replacerShort = (value, item) => value.replace(item[0], template('compare_short', repo, item[1], item[2]))
 
-    text = regex(text, /\[[\s\w\d`.-]+]\(https:\/\/github\.com\/([\w\d\-_]+)\/([\w\d\-_]+)\/compare\/([\w\d.-]+)\.\.\.([\w\d.-]+)\)/g, replacerFull)
-    text = regex(text, /https:\/\/github\.com\/([\w\d\-_]+)\/([\w\d\-_]+)\/compare\/([\w\d.-]+)\.\.\.([\w\d.-]+)/g, replacerFull)
-    text = regex(text, /([\w\d.-]+)\.\.\.([\w\d.-]+)/g, replacerShort)
+    text = regex(text, /\[[\s\w\d`.\-]+]\(https:\/\/github\.com\/([\w\d\-_]+)\/([\w\d\-_]+)\/compare\/([\w\d.\-]+)\.\.\.([\w\d.\-]+)\)/g, replacerFull)
+    text = regex(text, /https:\/\/github\.com\/([\w\d\-_]+)\/([\w\d\-_]+)\/compare\/([\w\d.\-]+)\.\.\.([\w\d.\-]+)/g, replacerFull)
+    text = regex(text, /([\w\d.\-]+)\.{3}([\w\d.\-]+)/g, replacerShort)
 
     return text
 }
