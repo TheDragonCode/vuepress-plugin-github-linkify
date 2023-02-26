d7bb091d7e942cf7c8688e1c4b026f6409eb1d8d
Andrey Helldar <helldar@dragon-code.pro>
2023-02-26 20:39:05 +0300

    Release v1.0.0-beta.12

--- lib/node/plugins/transformers/simple.d.ts
+++
@@ -1,3 +0,0 @@
-import type { GitHubLinkifyTransformer } from '../../types/transformer.js';
-export declare const simpleCompact: GitHubLinkifyTransformer;
-export declare const simpleExpand: GitHubLinkifyTransformer;
