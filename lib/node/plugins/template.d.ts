a34da596489b6f1d8a789e936dd12fadce58b8c4
Andrey Helldar <helldar@dragon-code.pro>
2023-02-26 18:51:37 +0300

    Release v1.0.0-beta.6

---
+++ lib/node/plugins/template.d.ts
@@ -0,0 +1,2 @@
+export declare const templateSeparator = "::";
+export declare const template: (key: string, ...args: string[]) => string;
---
+++ lib/node/plugins/transformers/simple.d.ts
@@ -0,0 +1,2 @@
+import type { GitHubLinkifyTransformer } from '../../types/transformer.js';
+export declare const simpleCompact: GitHubLinkifyTransformer;
