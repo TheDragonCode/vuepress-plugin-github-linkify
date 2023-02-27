a3615a959cb6c1e2953d3310ce3af0eb8bd468f9
Andrey Helldar <helldar@dragon-code.pro>
2023-02-27 16:22:19 +0300

    Release v1.0.13-beta.0

---
+++ lib/node/plugins/transformers/mention.d.ts
@@ -0,0 +1,3 @@
+import type { GitHubLinkifyTransformer } from '../../types/transformer.js';
+export declare const mentionCompact: GitHubLinkifyTransformer;
+export declare const mentionExpand: GitHubLinkifyTransformer;
--- lib/node/plugins/transformers/username.d.ts
+++
@@ -1,3 +0,0 @@
-import type { GitHubLinkifyTransformer } from '../../types/transformer.js';
-export declare const usernameCompact: GitHubLinkifyTransformer;
-export declare const usernameExpand: GitHubLinkifyTransformer;
