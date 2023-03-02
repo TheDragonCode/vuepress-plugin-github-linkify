14e0fc352db89cb5a3b138c03885a9d68c6270ca
Andrey Helldar <helldar@dragon-code.pro>
2023-03-02 11:52:08 +0300

    Removed unused files

--- src/node/plugins/template.ts
+++
@@ -1,5 +0,0 @@
-export const templateSeparator = '::'
-
-export const template = (key: string, ...args: string[]) => {
-    return templateSeparator + [key].concat(args).join(templateSeparator) + templateSeparator
-}
