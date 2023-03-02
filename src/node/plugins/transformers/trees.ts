88a827bec6c33b929f8a46059da462f1848c2fda
Andrey Helldar <helldar@dragon-code.pro>
2023-03-02 15:45:57 +0300

    Fixed: [plugin:vite:vue] Whitespace was expected.

--- src/node/plugins/transformers/trees.ts
+++ src/node/plugins/transformers/trees.ts
@@ -4,5 +4,5 @@ export const treesTransformer = Manager.create()
     .setKey('tree')
     .setCompactPatterns([
         /\[[\s`#@]*\d+]\(https:\/\/github\.com\/([\w\d\-_]+)\/([\w\d\-_]+)\/tree\/([\d\w.\-_\/]+)\)/g,
-        /https:\/\/github\.com\/([\w\d\-_]+)\/([\w\d\-_]+)\/tree\/([\d\w.\-_\/]+)/g
+        /(?<!:")(?<=^|\s)https:\/\/github\.com\/([\w\d\-_]+)\/([\w\d\-_]+)\/tree\/([\d\w.\-_\/]+)/g
     ])
